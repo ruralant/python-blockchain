77573357dd626361c1f044bd917012ba3ad00590
Antonio Rossi <info@antoniorossi.net>
2018-08-02 23:19:20 +0200

    console menu and basic blockchain verification added

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,98 @@
+# Initialising blockchain list
 blockchain = []
 
 
 def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain """
+    if len(blockchain) < 1:
+        return None
+
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchain value to the blockchain 
+
+    Arguments:
+        :transaction_amount: The amount that should be added.
+        :last_transaction: The last blockchain transaction (default [1])
+    """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-add_value(2)
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=0.9)
-add_value(10.89, get_last_blockchain_value())
+def get_transaction_value():
+    """ Returns the input of the user (a new transaction amount) as a float """
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
 
-print(blockchain)
+
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
+
+
+def print_blockchain_elements():
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+    else:
+        print('-' * 20)
+
+
+def verify_chain():
+    """ Verify if the new block contains exactly the previous block and first element """
+    # block_index = 0
+    is_valid = True
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            block_index += 1
+            continue
+        if blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+    # for block in blockchain:
+    #     if block_index == 0:
+    #         block_index += 1
+    #         continue
+    #     if block[0] == blockchain[block_index - 1]:
+    #         is_valid = True
+    #     else:
+    #         is_valid = False
+    #         break
+    #     block_index += 1
+    return is_valid
+
+
+waiting_for_input = True
+
+while waiting_for_input:
+    print('Please choose: ')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    elif user_choice == 'q':
+        waiting_for_input = False
+    else:
+        print('Invalid input. Please pick a valid value from the list')
+    if not verify_chain():
+        print_blockchain_elements()
+        print('Invalid blockchain!')
+        break
+else:
+    print('User Left!')
+
+print('Done!')

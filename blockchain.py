f8cbfb406b085bba911b271c93f7907b3ed3a8fa
Antonio Rossi <info@antoniorossi.net>
2018-08-04 22:44:00 +0200

    block mining and verification changed accodringly

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,16 @@
 # Initialising blockchain list
-blockchain = []
+genesis_block = {
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
+}
+blockchain = [genesis_block]
+open_transactions = []
+owner = 'Antonio'
+
+
+def hash_block(block):
+    return '-'.join([str(block[key]) for key in block])  # list comprehension
 
 
 def get_last_blockchain_value():
@@ -10,22 +21,42 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_transaction(transaction_amount, last_transaction=[1]):
+def add_transaction(recipient, sender=owner, amount=1.0):
     """ Append a new value as well as the last blockchain value to the blockchain 
 
     Arguments:
-        :transaction_amount: The amount that should be added.
-        :last_transaction: The last blockchain transaction (default [1])
+        :sender: the sender of the coin.
+        :recipient: the recipient of the coin.
+        :amount: the amount of coins sent (default = 1.0)
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+    transaction = {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount
+    }
+    open_transactions.append(transaction)
+
+
+def mine_block():
+    last_block = blockchain[-1]
+    hashed_block = hash_block(last_block)
+    # for key in last_block:
+    #     value = last_block[key]
+    #     hashed_block = hashed_block + str(value)
+
+    block = {
+        'previous_hash': hashed_block,
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float """
-    user_input = float(input('Your transaction amount please: '))
-    return user_input
+    recipient = input('Enter the recipient of the transaction:')
+    amount = float(input('Your transaction amount please: '))
+    return recipient, amount  # tuple
 
 
 def get_user_choice():
@@ -43,28 +74,12 @@ def print_blockchain_elements():
 
 def verify_chain():
     """ Verify if the new block contains exactly the previous block and first element """
-    # block_index = 0
-    is_valid = True
-    for block_index in range(len(blockchain)):
-        if block_index == 0:
-            block_index += 1
+    for (index, block) in enumerate(blockchain):
+        if index == 0:
             continue
-        if blockchain[block_index][0] == blockchain[block_index - 1]:
-            is_valid = True
-        else:
-            is_valid = False
-            break
-    # for block in blockchain:
-    #     if block_index == 0:
-    #         block_index += 1
-    #         continue
-    #     if block[0] == blockchain[block_index - 1]:
-    #         is_valid = True
-    #     else:
-    #         is_valid = False
-    #         break
-    #     block_index += 1
-    return is_valid
+        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+            return False
+    return True
 
 
 waiting_for_input = True
@@ -72,18 +87,27 @@ waiting_for_input = True
 while waiting_for_input:
     print('Please choose: ')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
+    print('2: Mine a new block')
+    print('3: Output the blockchain blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
-        tx_amount = get_transaction_value()
-        add_transaction(tx_amount, get_last_blockchain_value())
+        tx_data = get_transaction_value()
+        recipient, amount = tx_data  # tuple unpack
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
-            blockchain[0] = [2]
+            blockchain[0] = {
+            'previous_hash': '',
+            'index': 0,
+            'transactions': [{'sender': 'Ash', 'recipient': 'Antonio', 'amount': 100}]
+        }
     elif user_choice == 'q':
         waiting_for_input = False
     else:
